491055867fdda7bb7e7e8cdda78bb791253d697f
Almanac <ruoyw0@gmail.com>
2021-02-16 12:04:57 -0800

    Filled in wallet initialization with coins. Now Just need to do GatherCoinsFromWallet

--- SodaMachine/Wallet.cs
+++ SodaMachine/Wallet.cs
@@ -20,9 +20,24 @@ namespace SodaMachine
         //Fills wallet with starting money
         private void FillRegister()
         {
-          // At least 5 dollars in change. Going with $5.55
-          // 12 quarters = 3 dollars, 20 dimes = 2 dollars, 10 nickels = 50 cents, 5 pennies = 5 cents
-          
+            // At least 5 dollars in change. Going with $5.55
+            // 12 quarters = 3 dollars, 20 dimes = 2 dollars, 10 nickels = 50 cents, 5 pennies = 5 cents
+            for (int i = 0; i < 12; i++)
+            {
+                Coins.Add(new Quarter());
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Coins.Add(new Dime());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Coins.Add(new Nickel());
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Coins.Add(new Penny());
+            }
         }
     }
 }
